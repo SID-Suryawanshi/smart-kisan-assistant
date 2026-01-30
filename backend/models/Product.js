b510c7d0ea8ab2d862f8586dca690763e78a080a
SID-Suryawanshi <siddhantsurywanshi99@gmail.com>
2026-01-30 21:20:33 +0530

    added review feature

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -29,6 +29,17 @@ const productSchema = new mongoose.Schema({
     type: String,
   },
 
+  reviews: [
+    {
+      buyerId: { type: mongoose.Schema.Types.ObjectId, ref: "Buyer" },
+      rating: { type: Number, required: true, min: 1, max: 5 },
+      comment: { type: String },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  
+  averageRating: { type: Number, default: 0 },
+
   kisanId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Kisan",
